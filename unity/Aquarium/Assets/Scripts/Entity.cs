1c9522834335adad5ebef5e4c97ca6e507a4bf19
andersAsch0 <84699083+andersAsch0@users.noreply.github.com>
2025-04-06 20:43:31 -0700

    Update Entity.cs
    just had ReturnNearest return null to avoid the error

--- unity/Aquarium/Assets/Scripts/Entity.cs
+++ unity/Aquarium/Assets/Scripts/Entity.cs
@@ -17,10 +17,12 @@ public class Entity : MonoBehaviour
         
     }
 
-    public T FindClosest<T>() where T : new()
+    public T FindClosest<T>() where T : new() //get all objects of one type, then check their positions and return the closest
+
     {
-        var objs = GameObject.FindObjectsByType<T>(FindObjectsSortMode.None);
-        if(objs.Length == 0) return default(T);
-        return objs[0];
+        return(default(T));
+        //var objs = GameObject.FindObjectsByType<T>(FindObjectsSortMode.None);
+        //if(objs.Length == 0) return default(T);
+        //return objs[0];
     }
 }
