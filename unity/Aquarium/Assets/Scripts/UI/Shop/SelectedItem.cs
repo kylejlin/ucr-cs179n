3f3e026e42d07a30934508b8b246746a67e34e71
Menthy Wu <73259022+menthy-wu@users.noreply.github.com>
2025-04-14 12:08:15 -0700

    selected item preview working

--- unity/Aquarium/Assets/Scripts/UI/Shop/SelectedItem.cs
+++ unity/Aquarium/Assets/Scripts/UI/Shop/SelectedItem.cs
@@ -3,6 +3,8 @@ using UnityEngine.UI;
 
 public class SelectedItem : MonoBehaviour
 {
+    public GameObject modelPrefab; // todo: just a test
+    private GameObject modelInstance; // Instance of the 3D model
     private GameObject modelCamera; // Camera that renders the 3D model
     private RawImage rawImage;
     void Start()
@@ -12,19 +14,39 @@ public class SelectedItem : MonoBehaviour
 
         Camera cam = modelCamera.AddComponent<Camera>();
 
+
         // Set position and rotation (optional)
         modelCamera.transform.position = new Vector3(0, 3.6f, -5f);
         modelCamera.transform.rotation = Quaternion.Euler(50, 0, 0);
 
         // Customize the camera (optional)
         cam.clearFlags = CameraClearFlags.SolidColor;
-        cam.backgroundColor = Color.black;
+        cam.backgroundColor = new Color(0, 0, 0, 0);
         cam.orthographic = false;
+        cam.cullingMask = LayerMask.GetMask("ShopSelectedItem");
 
         RenderTexture rt = new RenderTexture(512, 512, 16);
         cam.targetTexture = rt;
         rawImage.texture = rt;
-        // modelInstance = Instantiate(modelPrefab, new Vector3(0, 2.8f, 0), Quaternion.identity);
+        modelInstance = Instantiate(modelPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        modelInstance.layer = LayerMask.NameToLayer("ShopSelectedItem");
+        SetLayerRecursively(modelInstance.transform, LayerMask.NameToLayer("ShopSelectedItem"));
 
     }
+    void Update()
+    {
+        if (modelInstance != null)
+        {
+            modelInstance.transform.Rotate(Vector3.up, 40f * Time.deltaTime); // 40 degrees per second
+        }
+    }
+    void SetLayerRecursively(Transform obj, int newLayer)
+    {
+        obj.gameObject.layer = newLayer;
+        foreach (Transform child in obj)
+        {
+            SetLayerRecursively(child, newLayer);
+        }
+    }
+
 }
