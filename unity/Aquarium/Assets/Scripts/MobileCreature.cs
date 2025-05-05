8f85ef0925ec87cf921afa3343af96757bea991c
xwRongx <57518285+xwRongx@users.noreply.github.com>
2025-05-05 13:25:46 -0700

    trilobite grows when eating

--- unity/Aquarium/Assets/Scripts/MobileCreature.cs
+++ unity/Aquarium/Assets/Scripts/MobileCreature.cs
@@ -11,16 +11,18 @@ public class MobileCreature : Creature
 {   
     
     public bool canSwim = false;
-    public int consumeRate = 10;
+    public float consumeRate = 20;
     public float speed = 1;
 
-    public static float huntingEnergyThreshold = 20;
+    public float huntingEnergyThreshold = 30;
     public static float metabolismRate = 1;
     public static float maxEatingDistance = 7;
 
     public BehaviorState state = BehaviorState.Idle;
 
     private Rigidbody mobileCreatureRB;
+
+    protected double breedingCooldown = 100;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -31,10 +33,13 @@ public class MobileCreature : Creature
         name = "Trilobite " + entityName;
         growthRate = 0.1f;
         adultEnergy = 40;
-        energy = 40;
-        maxEnergy = 40;
+        breedingCooldown = 100;
 
-        spawnSize = 1f; //for demo, have them spawn fully grown
+        spawnSize = .5f; //for demo, have them spawn fully grown
+        energy = maxEnergy = spawnSize*adultEnergy;
+        huntingEnergyThreshold = .75f * maxEnergy;
+        consumeRate = .5f * maxEnergy;
+
         spawnRadius = 5;
         minSpawnSpace = 1;
         minCMCubedPer = 2000;
@@ -45,6 +50,15 @@ public class MobileCreature : Creature
     void FixedUpdate()
     {
         energy -= Time.deltaTime * metabolismRate;
+
+        //if the creature is an adult, high energy, and past the breedingCooldown period, try breeding
+        if(getScale() == adultSize){
+            count += Time.deltaTime;
+            if((energy > adultEnergy*.75) && (count > breedingCooldown)){
+                count = 0;
+                tryDuplicate<MobileCreature>(minSpawnSpace, minCMCubedPer); 
+            }
+        }
 
         if (energy <= 0)
         {
@@ -122,6 +136,7 @@ public class MobileCreature : Creature
         {
             //Eat based on consumeRate 
             eat(closest.beingEaten(consumeRate));
+            grow(growthRate);
             return;
         }
 
@@ -156,6 +171,70 @@ public class MobileCreature : Creature
         if (shopMode) { Debug.LogWarning("Can't rotate in shop mode"); return; }
         //rotates creature with respect to front of creature (head points towards rotation)
         mobileCreatureRB.MoveRotation(Quaternion.LookRotation(angularVelocity, Vector3.forward));
+    }
+
+    /// <summary> scales up size and energy by the percentage passed in (wont exceed max size set) </summary>
+    public override void grow(float percentage)
+    {
+        if (maxEnergy + (adultEnergy * percentage) > adultEnergy)
+        {
+            setMaturity(1);
+        }
+        else if (maxEnergy + (adultEnergy * percentage) <= 0)
+        {
+            die();
+            Debug.LogWarning("Shrunk to death");
+        }
+        else
+        {
+            setMaturity(percentage + maxEnergy/adultEnergy);
+        }
+    }
+
+    /// <summary>
+    /// set growth to this size, keeping size and energy proportional (unlike grow, which ADDS the percentage to the current size). Percentage cant be negative
+    /// override creature setMaturity() function to add mobileCreature values that should scale
+    /// </summary>
+    public override void setMaturity(float percentage){
+        if(percentage <= 0) {Debug.LogWarning("maturity cannot be negative"); }
+        setScaleTo(adultSize * percentage); 
+        maxEnergy = percentage * adultEnergy;
+        huntingEnergyThreshold = .75f * maxEnergy;
+        consumeRate = .5f * maxEnergy;
+    }
+
+    /// <summary> make identical copy (for now, in a random position nearby. This is probably temporary). </summary>
+    public override void duplicate(Vector3 position) 
+    {
+        if (parentAquarium == null) { Debug.LogWarning("Could not find Aquarium parent"); return; }
+        if (!parentAquarium.isInBounds(position)) {return; } //keep w/in aquarium
+
+        parentAquarium.addEntity(this, position, transform.localRotation); //spawn nearby in same aquarium
+        //beingEaten(spawnSize*adultEnergy, false); //lose the same amount as the new creature spawned has
+    }
+
+    /// <summary> try to duplicate, but dont if there is a T too close to the attempted spawn location or too many of T in the aquarium as a whole. </summary>
+    public new void tryDuplicate<T>(float minSpace = 0, float minCMCubedPerT = 0) where T : Entity
+    {
+        Vector3 randVecNearby = new Vector3(Random.Range(-spawnRadius, spawnRadius), 0, Random.Range(-spawnRadius, spawnRadius)) + transform.localPosition; 
+        
+        T closestT = FindClosest<T>(randVecNearby); 
+        float closestTSqrDist =  Mathf.Infinity; 
+        if (closestT != default(T)) {closestTSqrDist = getSqrDistBw(FindClosest<T>(randVecNearby).transform.localPosition, randVecNearby); } 
+        
+        int numTInTank = getAllOfType<T>().Length;
+        float currCMCubedPerT = Mathf.Infinity;
+        if ((numTInTank > 0 )) { currCMCubedPerT = parentAquarium.volume() / numTInTank; }
+
+        //Debug.Log("currPos "+ transform.localPosition);
+        //Debug.Log("nrarbyPost "+ randVecNearby);
+        //Debug.Log("closest "+ closestTSqrDist);
+        //Debug.Log("curr units per T "+ currUnitsCubedPerT);
+        if ((closestTSqrDist > minSpace*minSpace) && (minCMCubedPerT < currCMCubedPerT))
+        {
+            print("trying to duplicate");
+            duplicate(randVecNearby);
+        }
     }
 
 }
