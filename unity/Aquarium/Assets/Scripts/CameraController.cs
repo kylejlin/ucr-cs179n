10cb1bba8f7f2cf83958419ed710c40f1a5d8ed4
Menthy Wu <73259022+menthy-wu@users.noreply.github.com>
2025-05-05 12:34:33 -0700

    fix camera

--- unity/Aquarium/Assets/Scripts/CameraController.cs
+++ unity/Aquarium/Assets/Scripts/CameraController.cs
@@ -11,7 +11,6 @@ public class CameraController : MonoBehaviour
     public float zoomSmoothTime = 0.3f; // Time to smooth the zooming effect
     public float movementSpeed = 5f; // Speed of camera movement
     public float distance = 150f; // Distance from the target
-    private bool isMoving = false; // Flag to check if the camera is already moving
 
     private Camera cam;
     private float targetFov;
@@ -39,11 +38,11 @@ public class CameraController : MonoBehaviour
 
         if (horizontal != 0 || vertical != 0)
         {
-            transform.RotateAround(target.position, Vector3.up, horizontal * rotationSpeed * Time.deltaTime);
-            transform.RotateAround(target.position, transform.right, vertical * rotationSpeed * Time.deltaTime);
+            transform.RotateAround(target.position, Vector3.up, horizontal * rotationSpeed * movementSpeed * Time.deltaTime);
+            transform.RotateAround(target.position, transform.right, vertical * rotationSpeed * movementSpeed * Time.deltaTime);
             if (transform.up.y < 0)
             {
-                transform.RotateAround(target.position, transform.right, -vertical * rotationSpeed * Time.deltaTime);
+                transform.RotateAround(target.position, transform.right, -vertical * rotationSpeed * Time.deltaTime * movementSpeed);
             }
         }
 
@@ -57,10 +56,6 @@ public class CameraController : MonoBehaviour
 
     }
 
-    void RotateCamera(Vector3 direction)
-    {
-        transform.RotateAround(target.position, direction, rotationSpeed * Time.deltaTime);
-    }
     public void setTarget(Transform newTarget)
     {
         target = newTarget;
@@ -69,29 +64,19 @@ public class CameraController : MonoBehaviour
 
     private IEnumerator SmoothMoveAndRotate()
     {
-        isMoving = true; // Start moving
 
         Vector3 targetPosition = new Vector3(target.position.x, target.position.y + distance, target.position.z);
         Quaternion targetRotation = Quaternion.Euler(90, 0, 0);
 
         // Smoothly move to the target position
-        while (Vector3.Distance(transform.position, targetPosition) > 1)
+        while (Vector3.Distance(transform.position, targetPosition) > 1 || Quaternion.Angle(transform.rotation, targetRotation) > 10)
         {
             transform.position = Vector3.Lerp(transform.position, targetPosition, movementSpeed * Time.deltaTime);
-            yield return null;
-        }
-        // Ensure we set the final position
-        transform.position = targetPosition;
-
-        // Smoothly rotate to the target rotation
-        while (Quaternion.Angle(transform.rotation, targetRotation) > 10)
-        {
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
             yield return null;
         }
-        // Ensure we set the final rotation
+        transform.position = targetPosition;
         transform.rotation = targetRotation;
 
-        isMoving = false; // Stop moving once we reach the target
     }
 }
