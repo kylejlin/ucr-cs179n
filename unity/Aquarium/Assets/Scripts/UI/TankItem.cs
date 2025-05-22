b21895ee66a223a4312beb87e57790b8ae711804
Menthy Wu <73259022+menthy-wu@users.noreply.github.com>
2025-05-22 13:55:11 -0700

    Tank Entity List highlights them

--- unity/Aquarium/Assets/Scripts/UI/TankItem.cs
+++ unity/Aquarium/Assets/Scripts/UI/TankItem.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class TankItem : Preview
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void setText(string text)
     {
         gameObject.transform.Find("Text").gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = text;
     }
 
+    public void Setup(Entity item, UnityAction onClick)
+    {
+        base.Setup(item);
+        gameObject.GetComponent<Button>().onClick.AddListener(onClick);
+
+    }
 }
+
